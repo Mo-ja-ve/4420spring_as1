50dff57c0cc926caa89d4674409ab1296b0eed4b
Mojave <39601026+Mo-ja-ve@users.noreply.github.com>
2021-03-08 21:48:16 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+
+int main(){
+
+  int pid;
+  int c;
+  std::string str;
+
+  struct dirent *ep;
+  DIR* dp;
+  FILE* read_piddir;
+  dp = opendir ("/proc");
+
+
+  if(dp != NULL){
+    while(ep = readdir (dp)){
+
+      pid = strtol (ep->d_name, NULL, 10);
+      if((ep->d_type == DT_DIR) && (pid > 0)){
+        //puts (ep->d_name);
+        char path_str[100] = { '/','p','r','o','c','/','\0'};
+        char add_stat[100] = { '/','s','t','a','t','\0'};
+        //char path_str[] = { "/proc/"};
+        //char add_stat[] = {  "/stat"};
+
+        strcat(path_str, ep->d_name);
+        strcat(path_str, add_stat);
+
+        read_piddir = fopen(path_str, "r");
+
+        if(read_piddir != NULL){
+        int temp_count  = 0;
+
+        while(1){
+          c = fgetc(read_piddir);
+          if(c == 32){
+            printf("         ");
+            temp_count++;
+          }
+
+          if(temp_count > 24){
+
+            break;
+          }
+
+          if(temp_count > 23){
+            printf("%c", c);
+          }
+
+          if(temp_count >= 4){
+            c = 0;
+          }
+
+          if( feof(read_piddir)){
+            break;
+          }
+          //std::cout<<str;
+          printf("%c", c);
+        }
+      }
+        printf("\n");
+      }
+    }
+    closedir(dp);
+  }else{
+    perror ("Couldn't open the directory");
+  }
+
+  return 0;
+}
